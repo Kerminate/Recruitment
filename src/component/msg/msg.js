99f67248aea5eca5b80d61bef58e9845274f4f91
Kerminate <kepeilin@outlook.com>
2018-06-12 11:48:06 +0800

    feat: sort unread messages

--- src/component/msg/msg.js
+++ src/component/msg/msg.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { List, Badge } from 'antd-mobile'
 
 @connect(
   state => state
 )
 class Msg extends Component {
+  getLast (arr) {
+    return arr[arr.length - 1]
+  }
   render () {
+    const Item = List.Item
+    const Brief = Item.Brief
+    const userid = this.props.user._id
+    const userinfo = this.props.chat.users
     const msgGroup = {}
     this.props.chat.chatmsg.forEach(v => {
       msgGroup[v.chatid] = msgGroup[v.chatid] || []
       msgGroup[v.chatid].push(v)
     })
-    console.log(msgGroup)
+    const chatList = Object.values(msgGroup).sort((a, b) => {
+      const aLast = this.getLast(a).create_time
+      const bLast = this.getLast(b).create_time
+      return bLast - aLast
+    })
     return (
       <div>
-        <h2>消息列表</h2>
+        <List>
+          { chatList.map((v) => {
+            const lastItem = this.getLast(v)
+            const targetId = v[0].from === userid ? v[0].to : v[0].from
+            const unreadNum = v.filter((v) => !v.read && v.to === userid).length
+            if (!userinfo[targetId]) {
+              return null
+            }
+            return (
+              <List key={lastItem._id}>
+                <Item
+                  extra={<Badge text={unreadNum}></Badge>}
+                  thumb={require(`../img/${userinfo[targetId].avatar}.png`)}
+                  arrow='horizontal'
+                  onClick={() => {
+                    this.props.history.push(`/chat/${targetId}`)
+                  }}
+                >
+                  { lastItem.content }
+                  <Brief>{ userinfo[targetId].name }</Brief>
+                </Item>
+              </List>
+            )
+          }) }
+        </List>
       </div>
     )
   }
